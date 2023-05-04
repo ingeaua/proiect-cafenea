333296c299d5b6c1e22e7a778285dc7803d16cb9
Alexandru Ingeaua <ingeaua.alexandru@gmail.com>
2023-05-04 14:34:44 +0300

    working on popup

--- scripts/acasa.js
+++ scripts/acasa.js
@@ -20,6 +20,23 @@ function setRandomHeart() {
 
 }
 
+function loadPopUp() {
+
+    const body = document.getElementsByTagName("body");
+    let copiiBody = body[0].children;
+    let popUp = document.getElementById("fundal-popup");
+    for (let i = 0; i < (copiiBody.length - 3); i++) {
+        copiiBody[i].classList.add("blurry");
+        console.log(copiiBody[i]);
+    }
+    popUp.classList.remove("ascuns");
+
+    /// to da sa mearga butonul de inchis popup
+
+}
+
 window.onload = setInterval(setRandomColor, 7500);
 
 window.onload = setInterval(setRandomHeart, 2000);
+
+window.onload = setTimeout(loadPopUp, 1000);
